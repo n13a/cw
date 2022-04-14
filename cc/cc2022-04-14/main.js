5060ab7eddb2e4abf6e9b1fa0646fa37d33591a1
Nima <nima97e@gmail.com>
2022-04-14 10:50:12 +0300

    Completed cc2022-04-14

---
+++ cc/cc2022-04-14/main.js
@@ -0,0 +1,18 @@
+// You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+// Complete the method which accepts such an array, and returns that single different number.
+
+// The input array will always be valid! (odd-length >= 3)
+
+// Examples
+// [1, 1, 2] ==> 2
+// [17, 17, 3, 17, 17, 17, 17] ==> 3
+
+function stray(numbers) {
+    numbers.sort()
+    if (numbers[0] != numbers[1]){
+      return numbers[0]
+    } else {
+      return numbers[numbers.length - 1]
+    }
+  }
