c2ab56ae4250c2b963fcc76852d9922040d16bb3
Nima <nima97e@gmail.com>
2022-04-27 13:58:18 +0300

    completed cc2022-04-27 (2)

---
+++ cc/cc2022-04-27/main2.js
@@ -0,0 +1,15 @@
+// Give you a function animal, accept 1 parameter:obj like this:
+
+// {name:"dog",legs:4,color:"white"}
+// and return a string like this:
+
+// "This white dog has 4 legs."
+// When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+// In the end, click "Submit" to submit your code pass this kata.
+
+
+
+function animal(obj){
+    return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`
+  }
