a454b1fa062e0153b6a1ccfd2ed494ac11793937
Nima <nima97e@gmail.com>
2022-04-28 11:50:15 +0300

    completed cc2022-04-28

---
+++ cc/cc2022-04-28/main.js
@@ -0,0 +1,6 @@
+// There is an object/class already created called MrFreeze. Mark this object as frozen so that no other changes can be made to it.
+
+
+
+// mark the MrFreeze object instance as frozen
+Object.freeze(MrFreeze)
