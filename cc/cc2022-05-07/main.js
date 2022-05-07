cac82a30d51535cdd850d3359c153d3e0298113f
Nima <nima97e@gmail.com>
2022-05-07 16:32:33 +0300

    complete cc2022-05-07 (2)

---
+++ cc/cc2022-05-07/main.js
@@ -0,0 +1,7 @@
+// Write a small function that returns the values of an array that are not odd.
+
+// All values in the array will be integers. Return the good values in the order they are given.
+
+
+
+const noOdds = v => v.filter(x=> x % 2 == 0)
---
+++ cc/cc2022-05-07/script.js
@@ -0,0 +1,18 @@
+// If　a = 1, b = 2, c = 3 ... z = 26
+
+// Then l + o + v + e = 54
+
+// and f + r + i + e + n + d + s + h + i + p = 108
+
+// So friendship is twice stronger than love :-)
+
+// The input will always be in lowercase and never be empty.
+
+
+function wordsToMarks(s){
+    let sum = 0
+    for(let i = 0; i < s.length; i++){
+      sum += s.charCodeAt(i) - 96
+    }
+    return sum
+  }
