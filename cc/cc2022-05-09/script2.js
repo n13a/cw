86c48d89f864338f22f862a695e364fdaf42b65c
Nima <nima97e@gmail.com>
2022-05-09 13:47:26 +0300

    complete cc2022-05-09 (2)

---
+++ cc/cc2022-05-09/script2.js
@@ -0,0 +1,8 @@
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+
+// input: names - unsorted array
+// output: sorted array
+const sortme = function( names ){
+    return names.sort()
+}
