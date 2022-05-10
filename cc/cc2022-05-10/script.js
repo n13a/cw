ecc37ee9c1a690da72cca6c6f470c92bb32fbc24
Nima <nima97e@gmail.com>
2022-05-10 11:28:09 +0300

    complete cc20222-05-10

---
+++ cc/cc2022-05-10/script.js
@@ -0,0 +1,10 @@
+// Write a function named repeater() that takes two arguments (a string and a number), and returns a new string where the input string is repeated that many times.
+
+// Example: (Input1, Input2 --> Output)
+// "a", 5 --> "aaaaa"
+
+
+
+function repeater(string, n){
+    return string.repeat(n)
+  }
