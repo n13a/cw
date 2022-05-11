ee8ad2b401b7a8f13c66e7e96cdb99406ce66eaf
Nima <nima97e@gmail.com>
2022-05-11 13:21:33 +0300

    complete cc2022-05-11

---
+++ cc/cc2022-05-11/script.js
@@ -0,0 +1,9 @@
+// Complete the keysAndValues function so that it takes in an object and returns the keys and values as separate arrays.
+
+// Example:
+
+// keysAndValues({a: 1, b: 2, c: 3}) // should return [['a', 'b', 'c'], [1, 2, 3]]
+// Style Points (JS/CoffeeScript only): This kata only tests for data that uses object literal notation (simple objects). For extra style, can you get your method to check for objects that extend their prototype?
+
+
+const keysAndValues = d => [Object.keys(d), Object.values(d)]
