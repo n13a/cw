af41f0525a6e9f3b20c3c869288c4e50ba5a9717
Nima <nima97e@gmail.com>
2022-05-17 11:13:27 +0300

    complete cc2022-05-17

---
+++ cc/cc2022-05-17/script.js
@@ -0,0 +1,5 @@
+// Complete the code which should return true if the given object is a single ASCII letter (lower or upper case), false otherwise.
+
+String.prototype.isLetter = function() {
+    return (/^[a-zA-Z]$/g).test(this);
+}
