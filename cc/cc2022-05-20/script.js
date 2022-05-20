c38836c696f2c63846783cff11ca71b1b8cced71
Nima <nima97e@gmail.com>
2022-05-20 12:16:19 +0300

    complete cc2022-05-20

---
+++ cc/cc2022-05-20/script.js
@@ -0,0 +1,11 @@
+// Here we have a function that help us spam our hearty laughter. But is not working! I need you to find out why...
+
+// Expected results:
+
+// spam(1);  // hue
+// spam(6);  // huehuehuehuehuehue
+// spam(14); // huehuehuehuehuehuehuehuehuehuehuehuehuehue
+
+function spam(number){
+    return 'hue'.repeat(number)
+  }
