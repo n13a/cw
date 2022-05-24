a366cbb331d30f32081fba8534491ba335141511
Nima <nima97e@gmail.com>
2022-05-24 16:34:14 +0300

    complete cc2022-05-24 (4)

---
+++ cc/cc2022-05-24/script4.js
@@ -0,0 +1,5 @@
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+
+
+const removeExclamationMarks = s => s.split('').filter(x=> x != '!').join('')
