34319e0db0438f077bdeb153c5ad725cae34ef97
Nima <nima97e@gmail.com>
2022-05-26 12:23:48 +0300

    reformat cc2022-05-26 (2)

--- cc/cc2022-05-26/script2.js
+++ cc/cc2022-05-26/script2.js
@@ -2,11 +2,5 @@
 
 // Why doesn't greet_abe() actually greet Abe?
 
-let name = 'Abe';
-let greet_abe = function() {
-  return "Hello, " + name + '!';
-};
-let nameBen = 'Ben';
-let greet_ben = function() {
-  return "Hello, " + nameBen + '!';
-};
+let greet_abe = _=> 'Hello, Abe!'
+let greet_ben = _=> 'Hello, Ben!'
