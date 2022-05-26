3fa849e228556c857f80ec35c00e0bcafeb68d6e
Nima <nima97e@gmail.com>
2022-05-26 12:24:44 +0300

    refactor cc2022-05-26 (2)

--- cc/cc2022-05-26/script2.js
+++ cc/cc2022-05-26/script2.js
@@ -2,5 +2,5 @@
 
 // Why doesn't greet_abe() actually greet Abe?
 
-let greet_abe = _=> 'Hello, Abe!'
-let greet_ben = _=> 'Hello, Ben!'
+let greet_abe = _=> 'Hello, Abe!';
+let greet_ben = _=> 'Hello, Ben!';
