2629859512b66e90db87047b7eae8016692d5218
Nima <nima97e@gmail.com>
2022-05-28 12:13:44 +0300

    complete cc2022-05-28

---
+++ cc/cc2022-05-28/script.js
@@ -0,0 +1,13 @@
+// max diff - easy
+
+
+// ou must implement a function that returns the difference between the largest and the smallest value in a given list / array (lst) received as the parameter.
+
+// lst contains integers, that means it may contain some negative numbers
+// if lst is empty or contains a single element, return 0
+// lst is not sorted
+// [1, 2, 3, 4]   //  returns 3 because 4 -   1  == 3
+// [1, 2, 3, -4]  //  returns 7 because 3 - (-4) == 7
+// Have fun!
+
+const maxDiff = l => l.length > 1 ? l.sort((a,b)=> a-b)[l.length - 1] - l[0] : 0
