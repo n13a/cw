721731f5a3e59e01fb3f1735e86b7e0f3075949b
Nima <nima97e@gmail.com>
2022-06-08 10:58:01 +0300

    complete

---
+++ cc/cc2022-06-08/sumOfTheFirstNthTermsOfSeries.js
@@ -0,0 +1,25 @@
+// Sum of the first nth term of Series
+
+
+// Task:
+// Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+// Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+// Rules:
+// You need to round the answer to 2 decimal places and return it as String.
+
+// If the given value is 0 then it should return 0.00
+
+// You will only be given Natural Numbers as arguments.
+
+// Examples:(Input --> Output)
+// 1 --> 1 --> "1.00"
+// 2 --> 1 + 1/4 --> "1.25"
+// 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+
+const SeriesSum = n=> {
+    for (var b = 0, i = 0; i < n; i++) {
+      b += 1 / (1 + i * 3)
+      }
+    return b.toFixed(2)
+  }
