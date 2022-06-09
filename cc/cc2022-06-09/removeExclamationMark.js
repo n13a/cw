347c351461aa67d2cae4b6601ad3144ac4e3e7cb
Nima <nima97e@gmail.com>
2022-06-09 10:19:35 +0300

    complete cc2022-06-09

---
+++ cc/cc2022-06-09/removeExclamationMark.js
@@ -0,0 +1,18 @@
+// Exclamation marks series #2: Remove all exclamation marks from the end of sentence
+
+
+
+// Description:
+// Remove all exclamation marks from the end of sentence.
+
+// Examples
+// remove("Hi!") === "Hi"
+// remove("Hi!!!") === "Hi"
+// remove("!Hi") === "!Hi"
+// remove("!Hi!") === "!Hi"
+// remove("Hi! Hi!") === "Hi! Hi"
+// remove("Hi") === "Hi"
+
+
+
+const remove = s=> s.replace(/!+$/, '')
