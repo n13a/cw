1029d7d6f8ef3cad42d20434d327985ffc845eab
Nima <nima97e@gmail.com>
2022-07-05 18:33:20 +0300

    complete cc2022-07-05

---
+++ cc/cc2022-07-05/findThePosition.js
@@ -0,0 +1,12 @@
+// Find the position!
+
+
+
+// When provided with a letter, return its position in the alphabet.
+
+// Input :: "a"
+
+// Ouput :: "Position of alphabet: 1"
+
+const position = letter => `Position of alphabet: ${letter.charCodeAt() - 96}`
+  
