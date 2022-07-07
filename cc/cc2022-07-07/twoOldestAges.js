d9bd2f46253ae658561c0ab2c7ab2f827094d7ed
Nima <nima97e@gmail.com>
2022-07-07 12:54:57 +0300

    complete cc2022-07-07

---
+++ cc/cc2022-07-07/twoOldestAges.js
@@ -0,0 +1,17 @@
+// Two Oldest Ages
+
+// The two oldest ages function/method needs to be completed. It should take an array of numbers as its argument and return the two highest numbers within the array. The returned value should be an array in the format [second oldest age, oldest age].
+
+// The order of the numbers passed in could be any order. The array will always include at least 2 items. If there are two or more oldest age, then return both of them in array format.
+
+// For example (Input --> Output):
+
+// [1, 2, 10, 8] --> [8, 10]
+// [1, 5, 87, 45, 8, 8] --> [45, 87]
+// [1, 3, 10, 0]) --> [3, 10]
+
+
+const twoOldestAges = ages=>{
+    ages.sort((a,b)=> b-a)
+    return [ages[1], ages[0]]
+  }
