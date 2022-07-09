756dfd347263fcbd863695cac494aa674441a052
Nima <nima97e@gmail.com>
2022-07-09 11:35:05 +0300

    complete cc2022-07-09

---
+++ cc/cc2022-07-09/digitize.js
@@ -0,0 +1,14 @@
+// Digitize
+
+// Given a non-negative integer, return an array / a list of the individual digits in order.
+
+// Examples:
+
+// 123 => [1,2,3]
+
+// 1 => [1]
+
+// 8675309 => [8,6,7,5,3,0,9]
+
+
+const digitize = n => `${n}`.split('').map(x=> Number(x))
