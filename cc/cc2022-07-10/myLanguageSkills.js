f44d9b6fce54e9f9ddb4a92fda872bd770eb678e
Nima <nima97e@gmail.com>
2022-07-10 11:11:57 +0300

    complete cc2022-07-10

---
+++ cc/cc2022-07-10/myLanguageSkills.js
@@ -0,0 +1,14 @@
+// My language skills
+
+// Task
+// You are given a dictionary/hash/object containing some languages and your test results in the given languages. Return the list of languages where your test score is at least 60, in descending order of the results.
+
+// Note: the scores will always be unique (so no duplicate values)
+
+// Examples
+// {"Java": 10, "Ruby": 80, "Python": 65}    -->  ["Ruby", "Python"]
+// {"Hindi": 60, "Dutch" : 93, "Greek": 71}  -->  ["Dutch", "Greek", "Hindi"]
+// {"C++": 50, "ASM": 10, "Haskell": 20}     -->  []
+
+
+const myLanguages = res => Object.keys(res).filter((x) => res[x] >= 60).sort((a, b) => res[b] - res[a])
