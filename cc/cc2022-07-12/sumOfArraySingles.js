90a4c08c479b5c8be8f7a5d69e5e03b653bd3ddb
Nima <nima97e@gmail.com>
2022-07-12 12:10:53 +0300

    complete cc2022-07-12

--- cc/cc2022-07-12/sumOfArraySingles.js
+++ cc/cc2022-07-12/sumOfArraySingles.js
@@ -5,6 +5,7 @@
 // For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15. Every other number occurs twice.
 
 function repeats(arr){
+
     const a = arr.reduce((o, i)=> {
       if(!o[i]) o[i] = 0
       o[i]++
