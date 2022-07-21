d27b6f8f1c411ef447a2d86e0d45325e475452fb
Nima <nima97e@gmail.com>
2022-07-21 19:31:00 +0300

    complete cc2022-07-21

---
+++ cc/cc2022-07-21/highestScoringWord.js
@@ -0,0 +1,21 @@
+// Highest Scoring Word
+
+// Given a string of words, you need to find the highest scoring word.
+
+// Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+// You need to return the highest scoring word as a string.
+
+// If two words score the same, return the word that appears earliest in the original string.
+
+// All letters will be lowercase and all inputs will be valid.
+
+
+const high = x=>{
+    const a = x.split(' ')
+    let array = []
+    a.forEach(x=> (array.push(x.split('').map(z=> z.charCodeAt() - 96)
+                  .reduce((a,c)=> a+ +c)))
+              )
+    return a[array.indexOf(Math.max(...array))]
+}
