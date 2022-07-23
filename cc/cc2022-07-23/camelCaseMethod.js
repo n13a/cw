7bc93905cf97ce17ac2ac1138756c0d82da44b7a
Nima <nima97e@gmail.com>
2022-07-23 10:57:23 +0300

    complete cc2022-07-23

---
+++ cc/cc2022-07-23/camelCaseMethod.js
@@ -0,0 +1,8 @@
+// CamelCase Method
+
+// Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or camelCase in Java) for strings. All words must have their first letter capitalized without spaces.
+
+// For instance:
+
+// "hello case".camelCase() => HelloCase
+// "camel case word".camelCase() => CamelCaseWord
