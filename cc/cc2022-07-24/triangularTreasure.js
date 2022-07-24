c1bd64ed31fc99c16bfa5979f512e6af17846b15
Nima <nima97e@gmail.com>
2022-07-24 11:20:58 +0300

    complete cc2022-07-24

---
+++ cc/cc2022-07-24/triangularTreasure.js
@@ -0,0 +1,19 @@
+// Triangular Treasure
+
+// Triangular numbers are so called because of the equilateral triangular shape that they occupy when laid out as dots. i.e.
+
+// 1st (1)   2nd (3)    3rd (6)
+// *          **        ***
+//            *         **
+//                      *
+// You need to return the nth triangular number. You should return 0 for out of range values:
+
+// For example: (Input --> Output)
+
+// 0 --> 0
+// 2 --> 3
+// 3 --> 6
+// -10 --> 0
+
+
+const triangular = n => n > 0 ? n * (n + 1) / 2 : 0
