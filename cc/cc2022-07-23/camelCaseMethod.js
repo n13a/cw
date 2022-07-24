6ef5bd522ef3ad0c997414b15f3f727a89b7d83f
Nima <nima97e@gmail.com>
2022-07-24 11:19:24 +0300

    refactor

--- cc/cc2022-07-23/camelCaseMethod.js
+++ cc/cc2022-07-23/camelCaseMethod.js
@@ -6,3 +6,10 @@
 
 // "hello case".camelCase() => HelloCase
 // "camel case word".camelCase() => CamelCaseWord
+
+
+String.prototype.camelCase = function(){
+    return this == '' ? '' : 
+    this.trim().split(' ')
+    .map(x=> x[0].toUpperCase() + x.slice(1)).join('')
+}
