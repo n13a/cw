8bf43a13fcb56785943298cbf29dcfd01036892d
Nima <nima97e@gmail.com>
2022-07-25 11:52:27 +0300

    complete cc2022-07-25

---
+++ cc/cc2022-07-25/alphabetWar.js
@@ -0,0 +1,47 @@
+// Alphabet War
+
+// Introduction
+// There is a war and nobody knows - the alphabet war!
+// There are two groups of hostile letters. The tension between left side letters and right side letters was too high and the war began.
+
+// Task
+// Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
+
+// The left side letters and their power:
+
+//  w - 4
+//  p - 3
+//  b - 2
+//  s - 1
+// The right side letters and their power:
+
+//  m - 4
+//  q - 3
+//  d - 2
+//  z - 1
+// The other letters don't have power and are only victims.
+
+// Example
+// alphabetWar("z");        //=> Right side wins!
+// alphabetWar("zdqmwpbs"); //=> Let's fight again!
+// alphabetWar("zzzzs");    //=> Right side wins!
+// alphabetWar("wwwwwwz");  //=> Left side wins!
+
+
+const val = {
+    w: 4,
+    p: 3,
+    b: 2,
+    s: 1,
+    m: -4,
+    q: -3,
+    d: -2,
+    z: -1
+}
+const alphabetWar = n=>{
+  const r = [...n].reduce((a,c)=> a + (val[c] || 0) ,0)
+  if(r == 0){
+    return "Let's fight again!"
+  }
+  return r > 0 ? 'Left side wins!' : "Right side wins!"
+}
