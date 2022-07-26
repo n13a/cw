a43ed046f31b2d2705420a813e79d2553e124658
Nima <nima97e@gmail.com>
2022-07-26 15:03:41 +0300

    complete cc2022-07-26

---
+++ cc/cc2022-07-26/isItANumber.js
@@ -0,0 +1,17 @@
+// Is It a Number?
+
+// Given a string s, write a method (function) that will return true if its a valid single integer or floating number or false if its not.
+
+// Valid examples, should return true:
+
+// isDigit("3")
+// isDigit("  3  ")
+// isDigit("-3.23")
+// should return false:
+
+// isDigit("3-4")
+// isDigit("  3   5")
+// isDigit("3 5")
+// isDigit("zero")
+
+const isDigit = s => s == parseFloat(s)
