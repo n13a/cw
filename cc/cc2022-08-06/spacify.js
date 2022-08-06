2c1edcaf7ed577673dda83472169b9659b1b91f9
Nima <nima97e@gmail.com>
2022-08-06 17:55:29 +0300

    complete cc2022-08-06

---
+++ cc/cc2022-08-06/spacify.js
@@ -0,0 +1,22 @@
+// Spacify
+
+// Modify the spacify function so that it returns the given string with spaces inserted between each character.
+
+// spacify("hello world") // returns "h e l l o   w o r l d"
+
+
+const spacify = str => [...str].join(' ')
+
+// Output
+
+// Time: 758ms Passed: 2Failed: 0
+// Test Results:
+// spacify
+// basic tests
+// Test Passed
+// Completed in 1ms
+// random tests
+// Test Passed
+// Completed in 2ms
+// Completed in 7ms
+// You have passed all of the tests! :)
