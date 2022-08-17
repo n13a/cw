7265945e8aad6fdd7d13055ebbc8210a009c2282
Nima <nima97e@gmail.com>
2022-08-17 15:39:23 +0430

    complete cc2022-08-17

---
+++ cc/cc2022-08-17/easyLogs.js
@@ -0,0 +1,17 @@
+// Easy Logs
+
+// Add two logs with base X, with the value of A and B. Example log A + log B where the base is X.
+
+const logs = (x , a, b)=> (Math.log(a) / Math.log(x))  + (Math.log(b) / Math.log(x))
+
+/* 
+Time: 718ms Passed: 2Failed: 0
+Test Results:
+Basic tests
+should pass basic tests
+Completed in 4ms
+Random tests
+should pass random tests
+Completed in 1ms
+You have passed all of the tests! :)
+*/
