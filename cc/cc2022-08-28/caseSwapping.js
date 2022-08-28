67188f297dc4c11879b8e8764bd383f86f09dc1f
Nima <nima97e@gmail.com>
2022-08-28 11:44:58 +0430

    complete cc2022-08-28

--- cc/cc2022-08-28/caseSwapping.js
+++ cc/cc2022-08-28/caseSwapping.js
@@ -9,3 +9,8 @@
 // "abc"        -->   "ABC"
 // "ABC"        -->   "abc"
 // "123235"     -->   "123235"
+
+
+const swap = str =>  str.split(' ').map(x=> [...x]
+    .map(y=> y.toLowerCase() == y ? y.toUpperCase() : 
+    y.toLowerCase()).join('')).join(' ')
