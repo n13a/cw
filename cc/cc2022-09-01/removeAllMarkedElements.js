4d768f7c40e72528c3ffcf8c6908eee6e16742b7
Nima <nima97e@gmail.com>
2022-09-01 09:09:50 +0430

    complete cc2022-09-01

---
+++ cc/cc2022-09-01/removeAllMarkedElements.js
@@ -0,0 +1,11 @@
+// Remove All The Marked Elements of a List
+
+// Define a method/function that removes from a given array of integers all the values contained in a second array.
+
+// Examples (input -> output):
+// * [1, 1, 2, 3, 1, 2, 3, 4], [1, 3] -> [2, 2, 4]
+// * [1, 1, 2, 3, 1, 2, 3, 4, 4, 3, 5, 6, 7, 2, 8], [1, 3, 4, 2] -> [5, 6, 7, 8]
+// * [8, 2, 7, 2, 3, 4, 6, 5, 4, 4, 1, 2, 3], [2, 4, 3] -> [8, 7, 6, 5, 1]
+
+
+Array.prototype.remove_ = (integer_list, values_list) => integer_list.filter(x=> !values_list.includes(x))
