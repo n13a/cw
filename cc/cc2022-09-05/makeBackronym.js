f5d8bb1cd5111b4b86b1894439f5705e71683666
Nima <nima97e@gmail.com>
2022-09-05 15:30:46 +0430

    complete cc2022-09-05(2)

---
+++ cc/cc2022-09-05/makeBackronym.js
@@ -0,0 +1,21 @@
+// makeBackronym
+
+
+// back·ro·nym
+// An acronym deliberately formed from a phrase whose initial letters spell out a particular word or words, either to create a memorable name or as a fanciful explanation of a word's origin.
+
+// "Biodiversity Serving Our Nation", or BISON
+
+// (from https://en.oxforddictionaries.com/definition/backronym)
+
+// Complete the function to create backronyms. Transform the given string (without spaces) to a backronym, using the preloaded dictionary and return a string of words, separated with a single space (but no trailing spaces).
+
+// The keys of the preloaded dictionary are uppercase letters A-Z and the values are predetermined words, for example:
+
+// dict["P"] == "perfect"
+// Examples
+// "dgm" ==> "disturbing gregarious mustache"
+
+// "lkj" ==> "literal klingon joke"
+
+const makeBackronym = str => [...str].map(x=> dict[x.toUpperCase()]).join(' ')
