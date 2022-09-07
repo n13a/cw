7a1cfdc3369504c828593084e4d592d7a655865e
Nima <nima97e@gmail.com>
2022-09-07 08:55:27 +0430

    create cc2022-09-07 folder

---
+++ cc/cc2022-09-07/largestElements.js
@@ -0,0 +1,12 @@
+// Largest Elements
+
+// Write a program that outputs the top n elements from a list.
+
+// Example:
+
+// largest(2, [7,6,5,4,3,2,1])
+// // => [6,7]
+
+function largest(n,xs){
+    
+}
