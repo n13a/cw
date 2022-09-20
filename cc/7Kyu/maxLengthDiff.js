588adf29201cdc11e7cd90e95780fbdae1ebf5a9
Nima <nima97e@gmail.com>
2022-09-20 20:56:18 +0430

    complete 7Kyu at 2022-09-20

---
+++ cc/7Kyu/maxLengthDiff.js
@@ -0,0 +1,24 @@
+// Maximum Length Difference
+
+// You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z. Let x be any string in the first array and y be any string in the second array.
+
+// Find max(abs(length(x) − length(y)))
+
+// If a1 and/or a2 are empty return -1 in each language except in Haskell (F#) where you will return Nothing (None).
+
+// Example:
+// a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+// a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+// mxdiflg(a1, a2) --> 13
+// Bash note:
+// input : 2 strings with substrings separated by ,
+// output: number as a string
+
+
+const mxdiflg = (a1, a2)=>{
+    if(a1.length == 0 || a2.length == 0) return -1
+    const funcM = (x)=> x.length
+    const a1Map = a1.map(funcM)
+    const a2Map = a2.map(funcM)
+    return Math.max(Math.max(...a1Map) - Math.min(...a2Map), Math.max(...a2Map) - Math.min(...a1Map))
+}
