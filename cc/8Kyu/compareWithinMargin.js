a4f8195fbbb18e57fa32b7603349d89620997c5d
Nima <nima97e@gmail.com>
2022-09-27 21:09:35 +0330

    complete 8Kyu at 2022-09-27

---
+++ cc/8Kyu/compareWithinMargin.js
@@ -0,0 +1,34 @@
+// Compare within margin
+
+// Create a function close_compare that accepts 3 parameters: a, b, and an optional margin. The function should return whether a is lower than, close to, or higher than b.
+
+// a is considered "close to" b if margin is greater than or equal to the distance between a and b.
+
+// Please note the following:
+
+//     When a is close to b, return 0.
+
+// Otherwise...
+
+//     When a is less than b, return -1.
+
+//     When a is greater than b, return 1.
+
+// If margin is not given, treat it as zero.
+
+// Assume: margin >= 0
+
+// Tip: Some languages have a way to make parameters optional.
+// Example 1
+
+// If a = 3, b = 5, and margin = 3, then close_compare(a, b, margin) should return 0.
+
+// This is because a and b are no more than 3 numbers apart.
+// Example 2
+
+// If a = 3, b = 5, and margin = 0, then close_compare(a, b, margin) should return -1.
+
+// This is because the distance between a and b is greater than 0, and a is less than b.
+
+
+const closeCompare = (a, b, margin = 0)=> margin >= Math.abs(a - b) ? 0 : a < b ? -1 : 1
