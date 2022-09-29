855627c6a7572165e4ed49079393fe8a609eefbe
Nima <nima97e@gmail.com>
2022-09-29 12:32:45 +0330

    complete 6Kyu at 2022-09-29

---
+++ cc/6Kyu/dataReverse.js
@@ -0,0 +1,32 @@
+// Data Reverse
+
+// A stream of data is received and needs to be reversed.
+
+// Each segment is 8 bits long, meaning the order of these segments needs to be reversed, for example:
+
+// 11111111  00000000  00001111  10101010
+//  (byte1)   (byte2)   (byte3)   (byte4)
+
+// should become:
+
+// 10101010  00001111  00000000  11111111
+//  (byte4)   (byte3)   (byte2)   (byte1)
+
+// The total number of bits will always be a multiple of 8.
+
+// The data is given in an array as such:
+
+// [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
+
+// Note: In the C and NASM languages you are given the third parameter which is the number of segment blocks.
+
+
+function dataReverse(data) {
+    let array = []
+    const c = 8;
+    for (let i = 0; i < data.length; i += c) {
+      const chunk = data.slice(i, i + c)
+      array.push(chunk)
+    }
+    return [].concat.apply([], array.reverse());
+}
