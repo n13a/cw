244e3c7e9783c463393bef27d646430e6b6722bb
Nima <nima97e@gmail.com>
2022-10-05 15:04:58 +0330

    solve 7Kyu at 2022-10-05

---
+++ cc/7Kyu/getDecimal.js
@@ -0,0 +1,12 @@
+// Get decimal part of the given number
+
+// Write a function that returns only the decimal part of the given number.
+
+// You only have to handle valid numbers, not Infinity, NaN, or similar. Always return a positive decimal part.
+// Examples
+
+// getDecimal(2.4)  === 0.4
+// getDecimal(-0.2) === 0.2
+
+
+const getDecimal = n=> Math.abs(n % 1)
