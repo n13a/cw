fb7296a5e842a0ce907ebd378ec1d5f70aafc531
Nima <46765718+n13a@users.noreply.github.com>
2022-10-08 15:55:34 +0330

    solve 7Kyu at 2022-10-08

---
+++ cc/7Kyu/checkForPrimeNums.js
@@ -0,0 +1,24 @@
+// Check for prime Numbers
+
+// In this kata you will create a function to check a non-negative input to see if it is a prime number.
+
+// The function will take in a number and will return True if it is a prime number and False if it is not.
+
+// A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+// Examples(input --> output)
+
+// 0 --> false
+// 1 --> false
+// 2 --> true
+// 11 --> true
+// 12 --> false
+
+const isPrime = n => {
+    for(let i = 2, s = Math.sqrt(n); i <= s; i++){
+      if (n % i == 0){
+        return false
+      }
+    }
+    return n > 1
+}
+  
