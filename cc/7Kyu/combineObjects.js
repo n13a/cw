46f659c6a901131ebfc9e42e0c318a181ae96244
Nima <nima97e@gmail.com>
2022-10-13 11:51:52 +0330

    solve 7Kyu at 2022-10-13

---
+++ cc/7Kyu/combineObjects.js
@@ -0,0 +1,22 @@
+// Combine Objects
+
+// Your task is to write a function that takes two or more objects and returns a new object which combines all the input objects.
+
+// All input object properties will have only numeric values. Objects are combined together so that the values of matching keys are added together.
+
+// An example:
+
+// const objA = { a: 10, b: 20, c: 30 }
+// const objB = { a: 3, c: 6, d: 3 }
+// combine(objA, objB) // Returns { a: 13, b: 20, c: 36, d: 3 }
+
+// The combine function should be a good citizen, so should not mutate the input objects.
+
+
+const combine = (...obj)=>
+  obj.reduce((a, c)=> {
+    for(let i in c){
+      a[i] ? a[i] += c[i] : a[i] = c[i]
+      }
+    return a
+    }, {})
