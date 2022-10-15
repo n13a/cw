15b3e749397b634b4d396b595868f4d853a0b917
Nima <nima97e@gmail.com>
2022-10-15 10:59:59 +0330

    solve 6Kyu at 2022-10-15

---
+++ cc/6Kyu/replaceWithAlphabetPosition.js
@@ -0,0 +1,18 @@
+// Replace With Alphabet Position
+
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+// Example
+
+// alphabetPosition("The sunset sets at twelve o' clock.")
+
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+
+const alphabetPosition = t =>{
+    const a = t.toLowerCase().match(/[a-z]/gi)   
+    return a === null ? '' : a.map(x=> x.charCodeAt() - 96).join(' ') 
+}
