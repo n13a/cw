9dabdd90a119b9dc04dbff02e99a03c842779a47
Nima <nima97e@gmail.com>
2022-11-06 19:59:57 +0330

    refactor

---
+++ cc/8Kyu/polishAlphabet.js
@@ -0,0 +1,36 @@
+// polish alphabet
+
+
+// There are 32 letters in the Polish alphabet: 9 vowels and 23 consonants.
+
+// Your task is to change the letters with diacritics:
+
+// ą -> a,
+// ć -> c,
+// ę -> e,
+// ł -> l,
+// ń -> n,
+// ó -> o,
+// ś -> s,
+// ź -> z,
+// ż -> z
+
+// and print out the string without the use of the Polish letters.
+
+// For example:
+
+// "Jędrzej Błądziński"  -->  "Jedrzej Bladzinski"
+
+const o = {
+    ą: 'a',
+    ć: 'c',
+    ę: 'e',
+    ł: 'l',
+    ń: 'n',
+    ó: 'o',
+    ś: 's',
+    ź: 'z',
+    ż: 'z'
+  }
+  
+const correctPolishLetters = string => [...string].map(x=> o[x] ? o[x] : x).join('')
