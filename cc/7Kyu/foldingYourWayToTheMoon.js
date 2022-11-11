ef792b1c24581235464ef4b50669d578a3f0c1a5
Nima <nima97e@gmail.com>
2022-11-11 11:41:29 +0330

    7Kyu

---
+++ cc/7Kyu/foldingYourWayToTheMoon.js
@@ -0,0 +1,18 @@
+// Folding your way to the moon
+
+
+// Have you heard about the myth that if you fold a paper enough times, you can reach the moon with it? Sure you have, but exactly how many? Maybe it's time to write a program to figure it out.
+
+// You know that a piece of paper has a thickness of 0.0001m. Given distance in units of meters, calculate how many times you have to fold the paper to make the paper reach this distance.
+// (If you're not familiar with the concept of folding a paper: Each fold doubles its total thickness.)
+
+// Note: Of course you can't do half a fold. You should know what this means ;P
+
+// Also, if somebody is giving you a negative distance, it's clearly bogus and you should yell at them by returning null (or whatever equivalent in your language). In Shell please return None. In C and COBOL please return -1.
+
+
+const foldTo = distance => {
+    let c = 0;
+    for (let i = 0.0001; i < distance; i *= 2) c += 1;
+    return distance < 0 ? null : c
+}
