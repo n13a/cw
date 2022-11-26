47ad9e96f09ad8a4101f824f61e213d1ca00a747
Nima <nima97e@gmail.com>
2022-11-26 11:30:30 +0330

    7Kyu | string merge

---
+++ cc/7Kyu/stringMerge.js
@@ -0,0 +1,14 @@
+// String Merge
+
+// Given two words and a letter, return a single word that's a combination of both words, merged at the point where the given letter first appears in each word. The returned word should have the beginning of the first word and the ending of the second, with the dividing letter in the middle. You can assume both words will contain the dividing letter.
+// Examples
+
+// ("hello", "world", "l")       ==>  "held"
+// ("coding", "anywhere", "n")   ==>  "codinywhere"
+// ("jason", "samson", "s")      ==>  "jasamson"
+// ("wonderful", "people", "e")  ==>  "wondeople"
+
+
+
+const stringMerge = (str1, str2, letter) => str1.substring(0, str1.indexOf(letter)) +
+    str2.substring(str2.indexOf(letter))
