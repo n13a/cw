5123cc96ece5fd4a5d3cc98c8a6fdbc2f65dc8b5
Nima <nima97e@gmail.com>
2022-12-01 13:14:07 +0330

    refactor

--- cc/6Kyu/reverseEveryOtherWordInTheString.js
+++ cc/6Kyu/reverseEveryOtherWordInTheString.js
@@ -5,6 +5,5 @@
 
 const reverse = str => {
     if (str == '   ') return ''
-    const res = str.split(' ').map((x, i) => i % 2 != 0 ? x.split('').reverse().join('') : x)
-    return res.join(' ')
+    return str.split(' ').map((x, i) => i % 2 != 0 ? x.split('').reverse().join('') : x).join(' ')
 }
