5eb94d1fbda9eb995eb59446f4ecfe75a04233f9
Nima <nima97e@gmail.com>
2022-12-04 11:29:24 +0330

    5Kyu | Mean Square Error

---
+++ cc/5Kyu/meanSquareError.js
@@ -0,0 +1,24 @@
+// Mean Square Error
+
+// Complete the function that
+
+//     accepts two integer arrays of equal length
+//     compares the value each member in one array to the corresponding member in the other
+//     squares the absolute value difference between those two values
+//     and returns the average of those squared absolute value difference between each member pair.
+
+// Examples
+
+// [1, 2, 3], [4, 5, 6]              -->   9   because (9 + 9 + 9) / 3
+// [10, 20, 10, 2], [10, 25, 5, -2]  -->  16.5 because (0 + 25 + 25 + 16) / 4
+// [-1, 0], [0, -1]                  -->   1   because (1 + 1) / 2
+
+
+
+const solution = (firstArray, secondArray) => {
+    let dif = []
+    firstArray.forEach((x, i) => {
+        dif.push(Math.pow(Math.abs(x - secondArray[i]), 2))
+    })
+    return dif.reduce((a, b) => a + +b, 0) / firstArray.length
+}
