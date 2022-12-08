db776f34dc6eb6dca8ca602d59f50609c3f6fea0
Nima <nima97e@gmail.com>
2022-12-08 10:40:44 +0330

    8Kyu | pick a set of first elements

---
+++ cc/8Kyu/pickASetOfFirstElements.js
@@ -0,0 +1,15 @@
+// pick a set of first elements
+
+// Write a function to get the first element(s) of a sequence. Passing a parameter n (default=1) will return the first n element(s) of the sequence.
+
+// If n == 0 return an empty sequence []
+// Examples
+
+// var arr = ['a', 'b', 'c', 'd', 'e'];
+// first(arr) //=> ['a'];
+// first(arr, 2) //=> ['a', 'b']
+// first(arr, 3) //=> ['a', 'b', 'c'];
+// first(arr, 0) //=> [];
+
+
+const first = (arr, n = 1) => arr.slice(0, n)
