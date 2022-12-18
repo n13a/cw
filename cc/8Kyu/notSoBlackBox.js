190a80994d66a313aaf36bec9d60268e253551bf
Nima <nima97e@gmail.com>
2022-12-18 13:44:53 +0330

    8Kyu | not so black box

---
+++ cc/8Kyu/notSoBlackBox.js
@@ -0,0 +1,28 @@
+// Not so black box
+
+// The kata has been preloaded with a function called blackBox.Figure out what the function does.
+
+// You may need to think laterally!
+
+// note: Use Object.prototype.toString() to log the function
+
+console.log(blackBox.toString())
+// output:
+e
+
+// function (args) {
+    //a secret message is hidden within me
+    //if you cannot see it, fear not!
+    //just think of another way to read me.
+
+//     if ("you have got this far, well done!");
+//     if ("you did NOT use toString(), let me know!");
+//     if ("you see this, you have my terrible secret...");
+//     if ("you call JSopenSesame(), you'll pass!");
+
+//     if (!args) return 'Empty?';
+//     if (typeof (args) == 'string') return 'Hmm...Interesting reading.';
+//     else return 'Ugh';
+// }
+
+JSopenSesame()
