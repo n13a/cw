6cc45910f27280da3d2aeae16bff199f780c9a82
Nima <nima97e@gmail.com>
2022-12-18 13:30:23 +0330

    8Kyu | sum squares

---
+++ cc/8Kyu/sumSquares.js
@@ -0,0 +1,20 @@
+// Sum Squares
+
+// Be Concise III - Sum Squares
+
+// You are given a program sumSquares that takes an array as input and returns the sum of the squares of each item in an array. For example:
+
+// sumSquares([1,2,3,4,5]) === 55 // 1 ** 2 + 2 ** 2 + 3 ** 2 + 4 ** 2 + 5 ** 2
+// sumSquares([7,3,9,6,5]) === 200
+// sumSquares([11,13,15,18,2]) === 843
+
+// Shorten the code such that it meets the requirements.
+
+// A few hints:
+
+//     Try researching about built-in Array methods; they may help shorten your code a lot
+
+// Good luck!
+
+
+sumSquares = ar => ar.reduce((a, c) => a + c ** 2, 0)
