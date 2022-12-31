a61cc766e624aaa5d2b98a6066654dd5e5dc87a6
Nima <nima97e@gmail.com>
2022-12-31 23:46:42 +0330

    7Kyu | see you next happy year (last kata of 2022)

---
+++ cc/7Kyu/seeYouNextHappyYear.js
@@ -0,0 +1,36 @@
+// See You Next Happy Year
+
+
+// Scenario
+// You're saying good-bye your best friend , See you next happy year .
+
+// Happy Year is the year with only distinct digits , (e.g) 2018
+
+// Task
+// Given a year, Find The next happy year or The closest year You'll see your best friend!alt!alt
+
+// Notes
+// Year Of Course always Positive .
+// Have no fear , It is guaranteed that the answer exists .
+// It's not necessary that the year passed to the function is Happy one .
+// Input Year with in range (1000  ≤  y  ≤  9000)
+// Input >> Output Examples:
+// nextHappyYear (7712) ==> return (7801)
+// Explanation:
+// As the Next closest year with only distinct digits is 7801 .
+
+// nextHappyYear (8989) ==> return (9012)
+// Explanation:
+// As the Next closest year with only distinct digits is 9012 .
+
+// nextHappyYear (1001) ==> return (1023)
+// Explanation:
+// As the Next closest year with only distinct digits is 1023 .
+
+
+const nextHappyYear = year => {
+    year++
+    const a = year + ''
+    if (Array.from(new Set(a)).join('') == a) return year
+    else return nextHappyYear(year)
+}
