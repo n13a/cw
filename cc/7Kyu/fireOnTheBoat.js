7076fd5cde5bd34b79d905f2f0128eb27e9ef6fe
Nima <nima97e@gmail.com>
2023-01-01 23:44:34 +0330

    7Kyu | fire on the boat

---
+++ cc/7Kyu/fireOnTheBoat.js
@@ -0,0 +1,12 @@
+// Fire on the boat
+
+// Enjoying your holiday, you head out on a scuba diving trip!
+
+// Disaster!! The boat has caught fire!!
+
+// You will be provided a string that lists many boat related items. If any of these items are "Fire" you must spring into action. Change any instance of "Fire" into "~~". Then return the string.
+
+// Go to work!
+
+
+const fireFight = s => s.split(' ').map(x => x == 'Fire' ? '~~' : x).join(' ')
