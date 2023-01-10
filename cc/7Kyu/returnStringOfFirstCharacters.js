2ab388e4ec87aa3f384a61e95d8ab77235a18bbe
Nima <nima97e@gmail.com>
2023-01-10 09:52:15 +0330

    7Kyu | return string of first characters

---
+++ cc/7Kyu/returnStringOfFirstCharacters.js
@@ -0,0 +1,12 @@
+// Return String of First Characters
+
+// In this exercise, a string is passed to a method and a new string has to be returned with the first character of each word in the string.
+
+// For example:
+
+// "This Is A Test" ==> "TIAT"
+
+// Strings will only contain letters and spaces, with exactly 1 space between words, and no leading/trailing spaces.
+
+
+const makeString = (s) => s.split(' ').map(x => x[0]).join('')
