53cc75b2e69909add17dbc69f13b2309694cd123
Nima <nima97e@gmail.com>
2023-01-13 23:21:21 +0330

    7Kyu | last digits of a number

---
+++ cc/7Kyu/lastDigitsOfANumber.js
@@ -0,0 +1,33 @@
+// last digits of a number
+
+// Your job is to implement a function which returns the last D digits of an integer N as a list.
+// Special cases:
+
+//     If D > (the number of digits of N), return all the digits.
+//     If D <= 0, return an empty list.
+
+// Examples:
+
+// N = 1
+// D = 1
+// result = [1]
+
+// N = 1234
+// D = 2
+// result = [3, 4]
+
+// N = 637547
+// D = 6
+// result = [6, 3, 7, 5, 4, 7]
+
+
+
+const lastDigit = (n, d) => {
+    const str = n.toString().split('').reverse()
+    if (str.length <= d) return n.toString().split('').map(Number)
+    let ar = []
+    for (let i = 0; i < d; i++) {
+        ar.push(str[i])
+    }
+    return ar.reverse().map(Number)
+}
