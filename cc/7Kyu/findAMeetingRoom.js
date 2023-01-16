ec195b80f68b1085a8f74e4c282da45721c266a7
Nima <nima97e@gmail.com>
2023-01-16 20:30:25 +0330

    7Kyu | find a meeting room

---
+++ cc/7Kyu/findAMeetingRoom.js
@@ -0,0 +1,12 @@
+// Find a Meeting Room
+
+// Your job at E-Corp is both boring and difficult. It isn't made any easier by the fact that everyone constantly wants to have a meeting with you, and that the meeting rooms are always taken!
+
+// In this kata, you will be given an array. Each value represents a meeting room. Your job? Find the first empty one and return its index (N.B. There may be more than one empty room in some test cases).
+
+// 'X' --> busy
+// 'O' --> empty
+
+// If all rooms are busy, return "None available!"
+
+const meeting = x => x.indexOf('O') === -1 ? 'None available!' : x.indexOf('O')
