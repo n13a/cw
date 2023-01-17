34d01c93f48a7430744147a0a3cc3d42053d7290
Nima <nima97e@gmail.com>
2023-01-17 20:46:50 +0330

    7Kyu | first class function factory

---
+++ cc/7Kyu/First-classFunctionFactory.js
@@ -0,0 +1,16 @@
+// First-Class Function Factory
+
+// Write a function, factory, that takes a number as its parameter and returns another function.
+
+// The returned function should take an array of numbers as its parameter, and return an array of those numbers multiplied by the number that was passed into the first function.
+
+// In the example below, 5 is the number passed into the first function. So it returns a function that takes an array and multiplies all elements in it by five.
+
+// Translations and comments (and upvotes) welcome!
+// Example
+
+// var fives = factory(5);       // returns a function - fives
+// var myArray = [1, 2, 3];
+// fives(myArray);               //returns [5, 10, 15];
+
+const factory = x => arr => arr.map(num => num * x)
