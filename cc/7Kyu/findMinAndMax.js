860a78130569d4cb2c19d3f51f0595cd3e033cd6
Nima <nima97e@gmail.com>
2023-01-20 22:57:42 +0330

    7Kyu | find min and max

---
+++ cc/7Kyu/findMinAndMax.js
@@ -0,0 +1,5 @@
+// Find min and max
+
+// Implement a function that returns the minimal and the maximal value of a list (in this order).
+
+const getMinMax = arr => [Math.min(...arr), Math.max(...arr)]
