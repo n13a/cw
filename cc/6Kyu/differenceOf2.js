6cd964ecacd3ef8de6eb63d097f4953d200380c6
Nima <nima97e@gmail.com>
2023-01-25 14:16:00 +0330

    6Kyu | difference of 2

---
+++ cc/6Kyu/differenceOf2.js
@@ -0,0 +1,20 @@
+// Difference of 2
+
+// The objective is to return all pairs of integers from a given array of integers that have a difference of 2.
+
+// The result array should be sorted in ascending order of values.
+
+// Assume there are no duplicate integers in the array. The order of the integers in the input array should not matter.
+// Examples
+
+// [1, 2, 3, 4]  should return [[1, 3], [2, 4]]
+
+// [4, 1, 2, 3]  should also return [[1, 3], [2, 4]]
+
+// [1, 23, 3, 4, 7] should return [[1, 3]]
+
+// [4, 3, 1, 5, 6] should return [[1, 3], [3, 5], [4, 6]]
+
+
+
+const twosDifference = input => input.filter(x => input.indexOf(x + 2) !== -1).map(z => [z, z + 2])
