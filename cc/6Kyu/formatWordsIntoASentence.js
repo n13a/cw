854d48ade6e3464a6b9836cd561c367cefcc2720
Nima <nima97e@gmail.com>
2023-01-26 23:03:01 +0330

    6Kyu | format words into a sentence

---
+++ cc/6Kyu/formatWordsIntoASentence.js
@@ -0,0 +1,22 @@
+// Format words into a sentence
+
+// Complete the method so that it formats the words into a single comma separated value. The last word should be separated by the word 'and' instead of a comma. The method takes in an array of strings and returns a single formatted string.
+
+// Note:
+
+//     Empty string values should be ignored.
+//     Empty arrays or null/nil/None values being passed into the method should result in an empty string being returned.
+
+// Example: (Input --> output)
+
+// ['ninja', 'samurai', 'ronin'] --> "ninja, samurai and ronin"
+// ['ninja', '', 'ronin'] --> "ninja and ronin"
+// [] -->""
+
+
+const formatWords = words => {
+    if (!words || words == '') return ''
+    const arr = words.filter(x => x !== '')
+    if (arr.length == 1) return arr[0]
+    return arr.slice(0, arr.length - 1).join(', ') + ` and ${arr[arr.length - 1]}`
+}
