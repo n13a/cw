ffe7b454c7e0441b070ca1bfd98d74e1a4744ad2
Nima <nima97e@gmail.com>
2023-01-27 12:44:00 +0330

    minor change

--- cc/6Kyu/formatWordsIntoASentence.js
+++ cc/6Kyu/formatWordsIntoASentence.js
@@ -17,6 +17,5 @@
 const formatWords = words => {
     if (!words || words == '') return ''
     const arr = words.filter(x => x !== '')
-    if (arr.length == 1) return arr[0]
-    return arr.slice(0, arr.length - 1).join(', ') + ` and ${arr[arr.length - 1]}`
+    return arr.length == 1 ? arr[0] : arr.slice(0, arr.length - 1).join(', ') + ` and ${arr[arr.length - 1]}`
 }
