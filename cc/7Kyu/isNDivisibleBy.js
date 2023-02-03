c05b8c9798ac3da2f3e6d4ba42ebea2682da1ea9
Nima <nima97e@gmail.com>
2023-02-03 09:48:35 +0330

    7Kyu | is n divisible by

---
+++ cc/7Kyu/isNDivisibleBy.js
@@ -0,0 +1,12 @@
+// Is n divisible by
+
+// Create a function isDivisible(n,...) that checks if the first argument n is divisible by all other arguments (return true if no other arguments)
+
+// Example:
+
+// isDivisible(6,1,3)--> true because 6 is divisible by 1 and 3
+// isDivisible(12,2)--> true because 12 is divisible by 2
+// isDivisible(100,5,4,10,25,20)--> true
+// isDivisible(12,7)--> false because 12 is not divisible by 7
+
+const isDivisible = (...args) => args.slice(1).every(x => args[0] % x === 0)
