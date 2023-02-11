a8cde0c6433f48e2b108c7a38f1b9c2bbcee20bf
Nima <nima97e@gmail.com>
2023-02-11 21:15:11 +0330

    7Kyu | four/seven

---
+++ cc/7Kyu/fourSeven.js
@@ -0,0 +1,11 @@
+// Four/Seven
+
+// Four Seven
+
+// Simple kata, simple rules: your function should accept the inputs 4 and 7. If 4 is entered, the function should return 7. If 7 is entered, the function should return 4. Anything else entered as input should return 0. There's only one catch, your function cannot include if statements, switch statements, or the ternary operator (or the eval function due to the fact that you can get around the if requirement using it).
+
+// There are some very simple ways of answering this problem, but I encourage you to try and be as creative as possible.
+
+
+const m = {4: 7, 7: 4}
+const fourSeven = n => m[n] || 0
