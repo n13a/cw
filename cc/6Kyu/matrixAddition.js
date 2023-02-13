b993604307befebb6898b23c323123ec0f9ac1e1
Nima <nima97e@gmail.com>
2023-02-13 20:17:17 +0330

    6Kyu | matrix addition

---
+++ cc/6Kyu/matrixAddition.js
@@ -0,0 +1,42 @@
+// Matrix Addition
+
+
+// Write a function that accepts two square matrices (N x N two dimensional arrays), and return the sum of the two. Both matrices being passed into the function will be of size N x N (square), containing only integers.
+
+// How to sum two matrices:
+
+// Take each cell [n][m] from the first matrix, and add it with the same [n][m] cell from the second matrix. This will be cell [n][m] of the solution matrix.
+
+// Visualization:
+
+// |1 2 3|     |2 2 1|     |1+2 2+2 3+1|     |3 4 4|
+// |3 2 1|  +  |3 2 3|  =  |3+3 2+2 1+3|  =  |6 4 4|
+// |1 1 1|     |1 1 3|     |1+1 1+1 1+3|     |2 2 4|
+
+// Example
+
+// matrixAddition(
+//   [ [1, 2, 3],
+//     [3, 2, 1],
+//     [1, 1, 1] ],
+//      +
+//   [ [2, 2, 1],
+//     [3, 2, 3],
+//     [1, 1, 3] ] )
+
+// returns:
+//   [ [3, 4, 4],
+//     [6, 4, 4],
+//     [2, 2, 4] ]
+
+const matrixAddition = (a, b) => {
+    let res = []
+    for (let i = 0; i < a.length; i++) {
+        let cur = []
+        for (let j = 0; j < a[i].length; j++) {
+            cur.push(a[i][j] + b[i][j])
+        }
+        res.push(cur)
+    }
+    return res
+}
