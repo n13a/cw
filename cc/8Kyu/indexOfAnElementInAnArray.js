6ec371d00821c304829a2cc11218fbbcd0678d88
Nima <nima97e@gmail.com>
2023-02-24 21:11:39 +0330

    8Kyu | index of an element in an array

---
+++ cc/8Kyu/indexOfAnElementInAnArray.js
@@ -0,0 +1,8 @@
+// Index of an element in an array
+
+// Task
+
+// Provided is a function find which accepts two parameters in the following order: array, element and returns the index of the element if found and "Not found" otherwise. Your task is to shorten the code as much as possible in order to meet the strict character count requirements of the Kata. (no more than 85) You may assume that all array elements are unique.
+
+
+const find = (a, e) => a.includes(e) ? a.indexOf(e) : 'Not found'
