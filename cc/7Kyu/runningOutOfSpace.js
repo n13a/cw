6b52c4dbed7c75b2ec9aa0bc1b623a7c6e58610d
Nima <nima97e@gmail.com>
2023-03-01 23:01:24 +0330

    7Kyu | running out of space

---
+++ cc/7Kyu/runningOutOfSpace.js
@@ -0,0 +1,14 @@
+// Running out of space
+// https://www.codewars.com/kata/56576f82ab83ee8268000059/train/javascript
+
+
+// Kevin is noticing his space run out! Write a function that removes the spaces from the values and returns an array showing the space decreasing.For example, running this function on the array['i', 'have', 'no', 'space'] would produce['i', 'ihave', 'ihaveno', 'ihavenospace'].
+
+function spacey(array) {
+    let str = ''
+    return array.reduce((a, c) => {
+        str += c
+        a.push(str)
+        return a
+    }, [])
+}
