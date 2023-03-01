304f3bd161370a92afa779a30bbf43c5cf309d64
Nima <nima97e@gmail.com>
2023-03-01 21:12:11 +0330

    7Kyu | password maker

---
+++ cc/7Kyu/passwordMaker.js
@@ -0,0 +1,22 @@
+// Password maker
+// https://www.codewars.com/kata/5637b03c6be7e01d99000046/train/javascript
+
+// One suggestion to build a satisfactory password is to start with a memorable phrase or sentence and make a password by extracting the first letter of each word.
+
+// Even better is to replace some of those letters with numbers (e.g., the letter O can be replaced with the number 0):
+
+//     instead of including i or I put the number 1 in the password;
+//     instead of including o or O put the number 0 in the password;
+//     instead of including s or S put the number 5 in the password.
+
+// Examples:
+
+// "Give me liberty or give me death"  --> "Gml0gmd"
+// "Keep Calm and Carry On"            --> "KCaC0"
+
+
+
+const o = { i: '1', o: '0', s: '5' }
+const makePassword = phrase => phrase.split(' ').reduce((a, c) => o[c[0].toLowerCase()] ?
+    a + o[c[0].toLowerCase()] : a + c[0]
+    , '')
