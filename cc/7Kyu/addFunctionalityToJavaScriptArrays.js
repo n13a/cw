1f587ae45b7dcf05ec930c20fc61412daa24c331
Nima <nima97e@gmail.com>
2023-03-01 23:20:26 +0330

    7Kyu | Adding useful functional functionality to JavaScript arrays

---
+++ cc/7Kyu/addFunctionalityToJavaScriptArrays.js
@@ -0,0 +1,19 @@
+// functionality to JavaScript arrays 
+// https://www.codewars.com/kata/52195c9bb576caf14200007f/train/javascript
+
+// The JavaScript standard now includes functional additions to array like map, filter and reduce, but sadly is missing the convenience functions range and sum . Implement a version of range and sum (which you can then copy and use in your future Kata to make them smaller).
+
+// Array.range(start, count) should return an array containing 'count' numbers from 'start' to 'start + count' Example: Array.range(0, 3) returns [0, 1, 2]
+
+// Array.sum() return the sum of all numbers in the array Example: [0, 1, 2].sum() returns 3 Example: Array.range(-1,4).sum() should return 2
+
+// While not forbidden try to write both function without using a for loop
+
+
+Array.range = function (start, count) {
+    return Array.from({ length: count }, (_, i) => i + start)
+}
+
+Array.prototype.sum = function () {
+    return this.reduce((a, c) => a + c, 0)
+}
