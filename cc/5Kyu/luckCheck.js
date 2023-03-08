df4b6aaccbc93df224f380f47829aa354ce12a47
Nima <nima97e@gmail.com>
2023-03-08 15:03:34 +0330

    5Kyu | luck check

---
+++ cc/5Kyu/luckCheck.js
@@ -0,0 +1,25 @@
+// Luck check
+// https://www.codewars.com/kata/5314b3c6bb244a48ab00076c/train/javascript
+
+// In some countries of former Soviet Union there was a belief about lucky tickets. A transport ticket of any sort was believed to posess luck if sum of digits on the left half of its number was equal to the sum of digits on the right half. Here are examples of such numbers:
+
+// 003111    #             3 = 1 + 1 + 1
+// 813372    #     8 + 1 + 3 = 3 + 7 + 2
+// 17935     #         1 + 7 = 3 + 5  // if the length is odd, you should ignore the middle number when adding the halves.
+// 56328116  # 5 + 6 + 3 + 2 = 8 + 1 + 1 + 6
+
+// Such tickets were either eaten after being used or collected for bragging rights.
+
+// Your task is to write a funtion luck_check(str), which returns true/True if argument is string decimal representation of a lucky ticket number, or false/False for all other numbers. It should throw errors for empty strings or strings which don't represent a decimal number.
+
+
+const reduce = a => a.reduce((a, c) => a + +c, 0)
+const luckCheck = ticket => {
+    if (/[A-Za-z|\s|-]/.test(ticket)) throw 'error'
+    const t = [...ticket]
+    const left = reduce(t.slice(0, (t.length / 2)))
+    if (t.length % 2 === 0) {
+        return left === reduce(t.slice((t.length / 2)))
+    }
+    return left === reduce(t.slice((t.length / 2) + 1))
+}
