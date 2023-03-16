92622279f4541c3d49aa1efa607a1540a36d35fe
Nima <nima97e@gmail.com>
2023-03-16 23:37:44 +0330

    7Kyu | function scope

---
+++ cc/7Kyu/functionScope.js
@@ -0,0 +1,12 @@
+// Function Scope
+// https://www.codewars.com/kata/56d344c7fd3a52566700124b/train/javascript
+
+
+// Write a function that adds from two invocations.
+
+// All inputs will be integers.
+
+// add(3)(4)  // 7
+// add(12)(20) // 32
+
+const add = x => z => x + z
