14a1efe213d8d05cd7852d5ad58e07daac6fbde0
Nima <nima97e@gmail.com>
2023-03-18 00:49:29 +0330

    7Kyu | Check three and two

---
+++ cc/7Kyu/checkThreeAndTwo.js
@@ -0,0 +1,22 @@
+// Check three and two
+// https://www.codewars.com/kata/5a9e86705ee396d6be000091/train/javascript
+
+
+// Given an array with exactly 5 strings "a", "b" or "c"(chars in Java, characters in Fortran), check if the array contains three and two of the same values.
+//     Examples
+
+// ["a", "a", "a", "b", "b"] ==> true  // 3x "a" and 2x "b"
+// ["a", "b", "c", "b", "c"] ==> false // 1x "a", 2x "b" and 2x "c"
+// ["a", "a", "a", "a", "a"] ==> false // 5x "a"
+
+
+
+function checkThreeAndTwo(array) {
+    const n = array.reduce((o, i) => {
+        if (!o[i]) o[i] = 0
+        o[i]++
+        return o
+    }, {})
+    const value = Object.values(n)
+    return value.includes(2) && value.includes(3)
+}
