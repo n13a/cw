fb01b6ca9b7e9c817279d7608013edeb367fee95
Nima <nima97e@gmail.com>
2023-04-06 21:54:47 +0330

    7Kyu | Between Extremes

---
+++ cc/7Kyu/betweenExtremes.js
@@ -0,0 +1,16 @@
+// Between Extremes
+// https://www.codewars.com/kata/56d19b2ac05aed1a20000430/train/javascripthttps://www.codewars.com/kata/56d19b2ac05aed1a20000430/train/javascript
+
+
+// Given an array of numbers, return the difference between the largest and smallest values.
+
+// For example:
+
+// [23, 3, 19, 21, 16] should return 20 (i.e., 23 - 3).
+
+// [1, 434, 555, 34, 112] should return 554 (i.e., 555 - 1).
+
+// The array will contain a minimum of two elements. Input data range guarantees that max-min will cause no integer overflow.
+
+
+const betweenExtremes = numbers => Math.max(...numbers) - Math.min(...numbers)
