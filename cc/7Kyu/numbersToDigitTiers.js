f265682e96f5f302b17363e7516f90a089c72bfe
Nima <nima97e@gmail.com>
2023-05-06 22:41:07 +0330

    7Kyu | numbers to digit tiers

---
+++ cc/7Kyu/numbersToDigitTiers.js
@@ -0,0 +1,20 @@
+/* Number to digit tiers
+Create a function that takes a number and returns an array of strings containing the number cut off at each digit.
+Examples
+
+    420 should return ["4", "42", "420"]
+    2017 should return ["2", "20", "201", "2017"]
+    2010 should return ["2", "20", "201", "2010"]
+    4020 should return ["4", "40", "402", "4020"]
+    80200 should return ["8", "80", "802", "8020", "80200"]
+
+PS: The input is guaranteed to be an integer in the range [0, 1000000] */
+
+
+function createArrayOfTiers(num) {
+  const str = num.toString(), arr = []
+  for(let i = 1; i <= str.length; ++i){   
+      arr.push(str.slice(0, i))
+    }
+  return arr;
+}
