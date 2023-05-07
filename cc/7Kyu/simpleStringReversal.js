da717d1d05e64f1bc69a1a68245ce2320690e4d0
Nima <nima97e@gmail.com>
2023-05-07 23:18:22 +0330

    7Kyu | simple string reversal

---
+++ cc/7Kyu/simpleStringReversal.js
@@ -0,0 +1,19 @@
+// Simple string reversal 
+// https://www.codewars.com/kata/5a8d1c82373c2e099d0000ac/train/javascript
+
+
+// In this Kata, you will be given a string and two indexes (a and b). Your task is to reverse the portion of that string between those two indices inclusive.
+
+// solve("codewars",1,5) = "cawedors" -- elements at index 1 to 5 inclusive are "odewa". So we reverse them.
+// solve("cODEWArs", 1,5) = "cAWEDOrs" -- to help visualize.
+
+// Input will be lowercase and uppercase letters only.
+
+// The first index a will always be lower that than the string length; the second index b can be greater than the string length. More examples in the test cases. Good luck!
+
+
+function solve(st, a, b) {
+    const mid = [...st.slice(a, b + 1)].reverse().join('')
+    const l = st.slice(0, a), r = st.slice(b + 1)
+    return l + mid + r
+}
