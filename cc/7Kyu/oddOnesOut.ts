53ff57cc1a62b1f635f6eccad3c1ea062d04458a
Nima <nima97e@gmail.com>
2023-05-14 23:18:48 +0330

    7Kyu | odd ones out

---
+++ cc/7Kyu/oddOnesOut.ts
@@ -0,0 +1,37 @@
+/* Odd Ones Out
+https://www.codewars.com/kata/5d376cdc9bcee7001fcb84c0/train/typescript
+
+
+The town sheriff dislikes odd numbers and wants all odd numbered families out of town! In town crowds can form and individuals are often mixed with other people and families. However you can distinguish the family they belong to by the number on the shirts they wear. As the sheriff's assistant it's your job to find all the odd numbered families and remove them from the town!
+
+Challenge: You are given a list of numbers. The numbers each repeat a certain number of times. Remove all numbers that repeat an odd number of times while keeping everything else the same.
+
+oddOnesOut([1, 2, 3, 1, 3, 3]) = [1, 1]
+
+In the above example:
+
+    the number 1 appears twice
+    the number 2 appears once
+    the number 3 appears three times
+
+2 and 3 both appear an odd number of times, so they are removed from the list. The final result is: [1,1]
+
+Here are more examples:
+
+oddOnesOut([1, 1, 2, 2, 3, 3, 3]) = [1, 1, 2, 2]
+oddOnesOut([26, 23, 24, 17, 23, 24, 23, 26]) = [26, 24, 24, 26]
+oddOnesOut([1, 2, 3]) = []
+oddOnesOut([1]) = []
+
+Are you up to the challenge?
+*/
+
+type objectMap = Record<string, number> 
+export function oddOnesOut(nums: number[]): number[] {
+  const o: objectMap = nums.reduce((ob: objectMap, i: number) => {
+    if(!ob[i]) ob[i] = 0
+    ob[i]++
+    return ob
+  }, {})
+  return nums.filter((n: number) => o[n] % 2 === 0)
+}
