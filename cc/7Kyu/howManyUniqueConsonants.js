c8b3f76c255bed6f5cbc69c5ff35cb12c33e4d1f
Nima <nima97e@gmail.com>
2023-05-18 22:57:40 +0330

    7Kyu | How Many Unique Consonants

---
+++ cc/7Kyu/howManyUniqueConsonants.js
@@ -0,0 +1,25 @@
+/* How Many Unique Consonants
+https://www.codewars.com/kata/5a19226646d843de9000007d/train/javascript
+
+Complete the function that counts the number of unique consonants in a string (made up of printable ascii characters).
+
+Consonants are letters used in English other than "a", "e", "i", "o", "u".
+
+Remember, your function needs to return the number of unique consonants - disregarding duplicates. For example, if the string passed into the function reads "add", the function should return 1 rather than 2, since "d" is a duplicate.
+
+Similarly, the function should also disregard duplicate consonants of differing cases. For example, "Dad" passed into the function should return 1 as "d" and "D" are duplicates.
+Examples
+
+"add" ==> 1
+"Dad" ==> 1
+"aeiou" ==> 0
+"sillystring" ==> 7
+"abcdefghijklmnopqrstuvwxyz" ==> 21
+"Count my unique consonants!!" ==> 7
+*/
+
+
+const countConsonants = str => { 
+  const s = str.toLowerCase().match(/[^aeiou\W\d]/g)
+  return [...new Set(s)].length
+}
