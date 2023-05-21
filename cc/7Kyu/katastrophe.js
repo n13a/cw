2fc6a6cc121b5bdf88fe06fca28f9009a2f162dc
Nima <nima97e@gmail.com>
2023-05-21 17:58:37 +0330

    7Kyu | katastrophe!

---
+++ cc/7Kyu/katastrophe.js
@@ -0,0 +1,23 @@
+/* Katastrophe
+https://www.codewars.com/kata/55a3cb91d1c9ecaa2900001b/train/javascript
+
+
+You have been employed by the Japanese government to write a function that tests whether or not a building is strong enough to withstand a simulated earthquake.
+
+A building will fall if the magnitude of the earthquake is greater than the strength of the building.
+
+An earthquake takes the form of a 2D-Array. Each element within the Outer-Array represents a shockwave, and each element within the Inner-Arrays represents a tremor. The magnitude of the earthquake is determined by the product of the values of its shockwaves. A shockwave is equal to the sum of the values of its tremors.
+
+Example earthquake --> [[5,3,7],[3,3,1],[4,1,2]] ((5+3+7) * (3+3+1) * (4+1+2)) = 735
+
+A building begins with a strength value of 1000 when first built, but this value is subject to exponential decay of 1% per year. For more info on exponential decay, follow this link - https://en.wikipedia.org/wiki/Exponential_decay
+
+Given an earthquake and the age of a building, write a function that returns "Safe!" if the building is strong enough, or "Needs Reinforcement!" if it falls.
+*/
+
+const strongEnough = (earthquake, age) => {
+  const strength = 1000 * (0.99) ** age
+  const quake = earthquake.reduce((a, c) =>
+    a * c.reduce((x, z) => x + z, 0), 1)
+  return quake < strength ? 'Safe!' : 'Needs Reinforcement!'
+}
