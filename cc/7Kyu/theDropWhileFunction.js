86a248d0b735a6141b9349198a62752644a2cc39
Nima <nima97e@gmail.com>
2023-06-22 23:44:42 +0330

    7Kyu | The dropWhile Function

---
+++ cc/7Kyu/theDropWhileFunction.js
@@ -0,0 +1,25 @@
+/* The dropWhile Function
+https://www.codewars.com/kata/54f9c37106098647f400080a/train/javascript
+
+
+Yet another staple for the functional programmer. You have a sequence of values and some predicate for those values. You want to remove the longest prefix of elements such that the predicate is true for each element. We'll call this the dropWhile function. It accepts two arguments. The first is the sequence of values, and the second is the predicate function. The function does not change the value of the original sequence.
+
+function isEven(num) {
+  return num % 2 === 0;
+}
+var seq = [2,4,6,8,1,2,5,4,3,2];
+
+dropWhile(seq, isEven) // -> [1,2,5,4,3,2]
+
+Your task is to implement the dropWhile function. If you've got a span function lying around, this is a one-liner! Alternatively, if you have a takeWhile function on your hands, then combined with the dropWhile function, you can implement the span function in one line. This is the beauty of functional programming: there are a whole host of useful functions, many of which can be implemented in terms of each other.
+*/
+
+
+function dropWhile(arr, pred) {
+    for (var i = 0; i < arr.length; ++i) {
+        if (!pred(arr[i])) {
+            break
+        }
+    }
+    return arr.slice(i)
+}
